a7b0716a8fd50683930a35b64d9443f5be48a52a
Vladimir Jigulin <mogaika@yandex.ru>
2016-02-06 05:47:50 -0600

    First WORKING binary. Fixed stack invalid pointer

--- src/boot/multiboot/loader.c
+++ src/boot/multiboot/loader.c
@@ -4,19 +4,11 @@
 #include <boot/multiboot/terminal.h>
 
 void multiboot_loader(uint32_t uMagic, multiboot_info_t *pMultiboot) {
-	/*terminal_initialize();
+	terminal_initialize();
 
 	terminal_setcolor(make_color(COLOR_WHITE, COLOR_BROWN));
 	terminal_clear();
 
 	terminal_printf("Multiboot struct place: 0x%p\n", pMultiboot);
-	terminal_printf("Multiboot magic: 0x%p\n", uMagic);*/
-	terminal_initialize();
-	while(1);
-
-	/*for (int i = 0; i < 42515; i+= 594) {
-		while (i > 53) {
-			i -= 5;
-		}
-	}*/
+	terminal_printf("Multiboot magic: 0x%p\n", uMagic);
 }
