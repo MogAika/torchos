250bd10d1cabb80f214170d3ce371a06dfc92d93
Vladimir Jigulin <mogaika@yandex.ru>
2016-02-12 04:06:30 -0600

    64 bit kernel loading preparing

---
+++ src/include/boot/kernel_input.h
@@ -0,0 +1,16 @@
+#ifndef KERNEL_INPUT_HEADER_H
+#define KERNEL_INPUT_HEADER_H 1
+
+typedef struct {
+	uint64_t size; // first bit - type, if 1, then can use, else cannot
+} kernel_input_phys_region_t;
+
+typedef struct {
+	uint64_t phys_memory_map_virt;
+	uint64_t total_memory_bytes;
+	uint32_t phys_regions_count;
+	uint32_t page_table_place_phys;
+	uint64_t page_table_place_virt;
+} kernel_input_t;
+
+#endif
